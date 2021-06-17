5ccf6d3b923ecfb1a17feb9989289e0a1aaef697
nalhuthaifi <85938096+nalhuthaifi@users.noreply.github.com>
2021-06-17 11:40:02 +0300

    Create Sedan.cs

---
+++ Sedan.cs
@@ -0,0 +1,39 @@
+
+
+
+public class Sedan : Car , VehicleWithRoof
+{
+    public Sedan(int ID,int Color, int SerialNumber, int KeyId,int NumOfPassengers, int NumOfDoors, String WheelsPower, String Fuel){
+        this.Id=ID;
+        this.Color=Color;
+        this.SerialNumber=SerialNumber;
+        this.NumOfPassengers=NumOfPassengers;
+        this.NumOfDoors=NumOfDoors;
+        this.WheelsPower=WheelsPower;
+        this.Fuel=Fuel;
+
+        System.Console.WriteLine("Sedan istructor is called!");
+
+    }
+    public  void MoveForward(){
+        System.Console.WriteLine("Move Forward!");
+    }
+    public  void Turn(String Direction){
+        System.Console.WriteLine($"Turn {Direction}!");
+    }
+    public  void Stop(){
+       System.Console.WriteLine("STOP!");
+    }
+    public void Switch(){
+       System.Console.WriteLine("switche on!");
+    }    
+    public bool OpenRoof(){
+        ///////////////////ASK ABOUT THIS
+          System.Console.WriteLine($"Sedan roof is ");
+    }
+    public  void toString(){
+       System.Console.WriteLine($"Sedan key type {KeyId}, bus Fuel type {Fuel}");
+    }
+    
+}
+
