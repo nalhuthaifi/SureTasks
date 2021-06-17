133cce2f67425101ceade31a2f1c97ca4315722a
nalhuthaifi <85938096+nalhuthaifi@users.noreply.github.com>
2021-06-17 11:11:54 +0300

    Create Car.cs

---
+++ Car.cs
@@ -0,0 +1,11 @@
+
+abstract class Car : Vehicle 
+{
+    public abstract int KeyId;
+    public abstract int NumOfPassengers;
+    public abstract int NumOfDoors;
+    public abstract String WheelsPower;
+    public abstract String Fuel;
+    public abstract void Switch();
+ //   public abstract bool AirbagFacilition();
+}
