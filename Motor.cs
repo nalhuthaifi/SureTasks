f399bf97e3ebde42c77d11fc767e58c0adffb5c6
nalhuthaifi <85938096+nalhuthaifi@users.noreply.github.com>
2021-06-20 09:21:18 +0300

    Update Motor.cs

--- Motor.cs
+++ Motor.cs
@@ -1,1 +1,30 @@
 
+
+public class Motor : Motorcycle 
+{
+    public Motor(int ID,int Color, int SerialNumber, int KeyId,int NumOfPassengers, int NumOfDoors, String WheelsPower, String Fuel){
+        this.Id=ID;
+        this.Color=Color;
+        this.SerialNumber=SerialNumber;
+        this.Fuel=Fuel;
+
+        System.Console.WriteLine("Motor istructor is called!");
+
+    }
+    public  void MoveForward(){
+        System.Console.WriteLine("Move Forward!");
+    }
+    public  void Turn(String Direction){
+        System.Console.WriteLine("Turn " + Direction);
+    }
+    public  void Stop(){
+       System.Console.WriteLine("STOP!");
+    }
+    public void Switch(){
+       System.Console.WriteLine("switche on!");
+    }    
+    public  void toString(){
+       System.Console.WriteLine($"Motor key type {KeyId}, bus Fuel type {Fuel}");
+    }
+    
+}
