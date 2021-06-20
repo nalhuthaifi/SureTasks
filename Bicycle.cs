163a0bec44f736b9bf15e4b85f1d193761fb4249
nalhuthaifi <85938096+nalhuthaifi@users.noreply.github.com>
2021-06-20 09:24:36 +0300

    Update Bicycle.cs

--- Bicycle.cs
+++ Bicycle.cs
@@ -1,4 +1,27 @@
-public class Bicycl : Motorcycle{
+public class Bicycle : Motorcycle{
+public Bicycle(int ID,int Color, int SerialNumber){
+        this.Id=ID;
+        this.Color=Color;
+        this.SerialNumber=SerialNumber;
 
 
+        System.Console.WriteLine("Bicycle istructor is called!");
+
+    }
+    public  void MoveForward(){
+        System.Console.WriteLine("Move Forward!");
+    }
+    public  void Turn(String Direction){
+        System.Console.WriteLine($"Turn {Direction}!");
+    }
+    public  void Stop(){
+       System.Console.WriteLine("STOP!");
+    }
+    public void Switch(){
+       System.Console.WriteLine("switche on!");
+    }    
+    public  void toString(){
+       System.Console.WriteLine($"Bicycle Id is " +Id);
+    }
+    
 }
